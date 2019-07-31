5206de2f6e912875d973ac559d2eeb076f13cdd7
MichaelNZ85 <michaelnz10@outlook.com>
2019-08-01 00:59:16 +1200

    Another test commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
   def new
-  @article = Article.new
+    @article = Article.new
   end
   
   def create
     @article = Article.new(article_params)
     @article.save
+    redirect_to articles_show(@article)
   end
 
   private
