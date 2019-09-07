172228cc60c654c3b09733e0b5db53a4f2b6f046
= <=>
2019-09-08 02:49:42 +1200

    Can log users in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
     @user = User.new
+  end
+
+  def edit 
+
   end
 
   def create
@@ -19,7 +23,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -28,14 +31,17 @@ class UsersController < ApplicationController
     end
   end
 
-    def show
-      @user = User.find(params[:id])
-      @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-    end
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
